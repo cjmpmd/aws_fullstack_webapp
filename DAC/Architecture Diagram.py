9512757b39ae305471ab14607fa1a5bf94d8125b
cjmpmd <71334484+cjmpmd@users.noreply.github.com>
2024-07-11 00:01:10 -0400

    Udated README and updated DAC

--- DAC/Architecture Diagram.py
+++ DAC/Architecture Diagram.py
@@ -1,39 +1,74 @@
 from diagrams import Cluster, Diagram
-from diagrams.aws.compute import ECS, EKS, Lambda
+from diagrams.aws.compute import EC2Instance, EKS, Lambda
 from diagrams.aws.database import Redshift
 from diagrams.aws.integration import SQS
 from diagrams.aws.storage import S3
+
+from diagrams.onprem.database import MySQL
 from diagrams.aws.network import Route53, ELB 
 from diagrams.programming.framework import Laravel
+from diagrams.onprem.storage import CEPH_OSD
+from diagrams.generic.virtualization import Virtualbox
+from diagrams.generic.os import Ubuntu
+
+from diagrams.generic.network import Firewall, Router
 
 
 def worker(x):
     wrk = []
     for i in range(x):
-        wrk.append(ECS("worker" + str(i+1)))
-    print(wrk)
+        wrk.append(EC2Instance("EC2"))
     return wrk
+
+
+with Diagram("Refactor App", show=False):
+
+    rtr = Router('Router')
+    fw = Firewall('FW')
+    # lb = ELB('LB')
+    vm = Virtualbox('Hypervisor t-1')
+
+    with Cluster("Virtual Machine"):
+        with Cluster("Services"):
+            workers = Ubuntu('OS')
+        with Cluster("DB"):
+            db = MySQL('State')
+        with Cluster("File Storage"):
+            storage = CEPH_OSD('Storage')
+            
+
+
+    rtr >> fw >> vm >> workers >> db
+    workers >> storage
+    db >> workers
+    storage >> workers
 
 
 with Diagram("Full Stack Web Architecture", show=False):
 
     dns = Route53('DNS Service')
     lb = ELB('LB')
-    source = EKS("k8s source")
 
-    with Cluster("AZ1"):
-        with Cluster("Event Workers"):
+    with Cluster("Virtual Machine"):
+        with Cluster("Services"):
             workers = worker(1)
+        with Cluster("DB"):
+            db = MySQL('State')
+        with Cluster("File Storage"):
+            storage = CEPH_OSD('Storage')
+            
 
 
-    dns >> lb >> source
-    source >> workers
+    dns >> lb >> workers >> db
+    workers >> storage
+    db >> workers
+    storage >> workers
 
     
 
 with Diagram("WebApp Architecture", show=False):
 
-    dns = Laravel('App')
+    lrvl = Laravel('App')
     # lb = ELB('LB')
     # source = EKS("k8s source")
 
@@ -42,5 +77,4 @@ with Diagram("WebApp Architecture", show=False):
     #         workers = worker(1)
 
 
-    # dns >> lb >> source
-    # source >> workers
+    lrvl 
--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Cloud Architecture
 ![Cloud Architecture](./DAC/full_stack_web_architecture.png)
 App Architecture
 ![Cloud Architecture](./DAC/webapp_architecture.png)
+
+
+### Dependencies
+#### Diagrams - Architecture diagrams
+- DAC (Diagrams as Code): The python package used for Architecure diagramming is [Diagrams by mingrammer](https://diagrams.mingrammer.com/).
+>(Note: when installing diagrams.mingrammer in Windows add the installation to PATH in environment variables as shown in this [tutorial](https://www.youtube.com/watch?v=XnxIfoUQeWw&ab_channel=AscendingDeer))
